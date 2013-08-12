5b9aae85f61beb604d0d140776bc7804cce0d648
Your New Author Name <Your New Author Email>
2013-08-12 13:55:44 -0400

    LEFified the signup code, create user now coincides with LEF database variables for user and placeholders for single sign on were added, social media removed

--- html/views/signup/config.php
+++ html/views/signup/config.php
@@ -1,3 +1,30 @@
 <?php
+$GLOBALS['title'] = 'Sign Up';
 
+if ($_SESSION['logged'] == true) { //<-index 'logged' apparently doesn't exist? See error.log
+    redirect('/login');
+}
+else if ($_POST != null) {
+
+    $user = new User();
+
+    $sql = "SELECT * FROM User u WHERE u.email = '" . $_POST['email'] . "' LIMIT 1";
+
+    $rs = Data::DB()->Execute($sql);
+    if (!$rs->EOF) {
+        redirect('signup/taken');
+    } else {
+        $user->createUser($_POST['userType'], $_POST['email'], $_POST['password'], $_POST['firstName'], $_POST['lastName'],$_POST['organization'], $_POST['phoneNo'], $_POST['phoneNoExt']. $_POST['address'], $_POST['city'], $_POST['postalCode'], $_POST['province'],$_POST['SSID']);
+        //Generate a RANDOM MD5 Hash for a password
+        $random_password = md5(uniqid(rand()));
+
+        $sql = 'UPDATE User SET password_recovery_timestamp=now(), verification_key="'.$random_password.'" WHERE email="'.$_POST['email'].'"';
+        $rs = Data::DB()->Execute($sql);
+
+        $message = '<h3>LEF Database</h3><p>Follow the link below to verify your email address.<br><a href="http://'.$_SERVER['HTTP_HOST'].'/verify/'.$random_password.'">http://'.$_SERVER['HTTP_HOST'].'/verify/'.$random_password.'</a><p>';
+        Send_Mail($_POST['email'], 'LEF Database Verify Email Address', $message);
+
+        redirect('signup/verify');
+    }
+}
 ?>
