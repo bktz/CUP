74dc432aed4ec8e4d829d93469ff8521b4626252
Your New Author Name <Your New Author Email>
2013-08-15 13:04:40 -0400

    implemented the search feature for projects by keywords

--- html/classes/project.php
+++ html/classes/project.php
@@ -11,20 +11,25 @@ class Project {
     private static $project = null;
 
     /**
+     * @var Array A group of one or more projects meeting search criteria all all their information.
+     */
+    private static $project = null;
+
+    /**
      *  @var Object Connection object that contains the prototype of the object (before update) so that we know what has changed when updating.
      */
     private static $rs = null;
 
     public function __construct() {
     }
-    
+
     /**
      * Retrieves a specific project from the database
      * @param unknown $project_id
      */
     public static function getProject($project_id){
-        $sql = "SELECT * FROM Project where project_id = ".$project_id;
-    
+        $sql = "SELECT * FROM Projects where project_id = ".$project_id;
+
         self::$rs = Data::DB()->Execute($sql);
         if (!self::$rs->EOF) {
             self::$project = self::$rs->fields;
@@ -34,5 +39,47 @@ class Project {
             self::$rs = null;
         }
     }
+
+    public static function getProjectList($searchString,$tags){
+
+        $regExpSearch = toRegExpString($searchString);
+        $sqlInner = "SELECT * FROM Projects p, ProjectInfo i, where p.project_id = i.project_id AND i.title REGEXP '".$regExpSearch."'";
+
+        $regExpTags = toRegExpList($searchString);
+        $sqlOuter = "SELECT * FROM tagged";
+
+        self::$rs = Data::DB()->Execute($sql);
+        if (!self::$rs->EOF) {
+            self::$project = self::$rs->fields;
+            return self::$project;
+        }
+        else {
+            self::$rs = null;
+        }
+    }
+
+    /**
+     * Takes in the search string and converts it to a regular expression for sql REGEXP '' calls, assumes string is delimited by spaces
+     * @param string $searchString the string to concatenate and delimit with '|'
+     * @return the regular expression matching the users search
+     */
+    public static function toRegExpString($searchString){
+
+        $searchString = preg_replace('!\s+!', '|', $searchString);
+        return $searchString;
+    }
+
+    /**
+     * Takes in a php list object and converts it to a regular expression for sql REGEXP '' calls
+     * @param unknown $tags
+     * @return unknown
+     */
+    public static function toRegExpList($tags){
+
+        $tags = implode('|', $tags);
+
+        return $tags;
+    }
+
 }
 ?>
